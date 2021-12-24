323fbab3105343d15a3814caa8ba302861cae65c
Mykola <mykola.danylo@izzi.software>
2021-12-24 11:55:26 +0200

    add SelenideWikiTests

---
+++ src/test/java/github/SelenideWikiTests.java
@@ -0,0 +1,40 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideWikiTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void shouldBeExampleOfCodeForJunit() {
+        open("/");
+        $$(".h1-mktg").first().shouldHave(text("Where the world builds software"));
+
+        $("[name=q]").setValue("Selenide").pressEnter();
+        $$(".f4").get(1).shouldHave(text("selenide/selenide"));
+
+        $$(".f4").get(1).click();
+        $("h1").shouldHave(text("selenide / selenide"));
+
+        $("#wiki-tab").click();
+        $(".markdown-body").shouldHave(text("Welcome to the selenide wiki!"));
+
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
